76b6be6449bce6cfb3641f852b8e9e358f0a2ca8
steamedEggMaster <cksdud5014@naver.com>
2024-07-29 12:20:47 +0900

    first commit

---
+++ src/main/java/com/example/muddyteam_chaemin/MuddyteamChaeminApplication.java
@@ -0,0 +1,13 @@
+package com.example.muddyteam_chaemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuddyteamChaeminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuddyteamChaeminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/muddyteam_chaemin/MuddyteamChaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muddyteam_chaemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuddyteamChaeminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
